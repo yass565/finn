bc5584e5a8d57466e4300a040379e60f359067e3
unknown <oussama.elharmali@gmail.com>
2022-02-24 03:24:04 +0100

    authUpdate

--- app/Http/Controllers/Api/Auth/AuthUserController.php
+++ app/Http/Controllers/Api/Auth/AuthUserController.php
@@ -7,7 +7,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class AuthController extends Controller
+class AuthUserController extends Controller
 {
     public function login(Request $req ){
         $this->validate($req,
